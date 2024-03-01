eb94123e5f2086be2a782c48bd3a9bd0146e15cd
star1909 <70825609+star1909@users.noreply.github.com>
2024-03-01 01:58:23 +0000

    treasure-island finished

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ print("Your mission is to find the treasure.")
 #Write your code below this line 👇
 
 # conditions for the game over : right, swim, blue.
-crossroad = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"')
+crossroad = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"\n')
 if crossroad == 'left':
-  lake = input('You come to a lake. There is an island in the middle of the lake. type "wait" to wait for a boat. Type "swim" to swim across.')
+  lake = input('You come to a lake. There is an island in the middle of the lake. type "wait" to wait for a boat. Type "swim" to swim across.\n')
   if lake == "wait":
-    door = input('You arrive at the island unharmed. There is a house with three doors. One yellow, one red and one blue. Which color do you choose?')
+    door = input('You arrive at the island unharmed. There is a house with three doors. One yellow, one red and one blue. Which color do you choose?\n')
     if door == 'red':
       print("It's a room full of fire. Game over.")
     elif door == 'yellow':
